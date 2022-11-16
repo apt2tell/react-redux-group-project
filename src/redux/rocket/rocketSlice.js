c614b36288521ba0633b7a80306b860a3eac7d2c
Okechukwu <okei.muokwugwo@outlook.com>
2022-11-16 12:17:52 +0100

    Update rockets reducer and extraReducers function

--- src/redux/rocket/rocketSlice.js
+++ src/redux/rocket/rocketSlice.js
@@ -9,7 +9,7 @@ const APIurl = 'https://api.spacexdata.com/v3/rockets';
 
 // Async Action Thunk
 export const getRocketAPI = createAsyncThunk(
-  'rockets/getRockets',
+  'rockets/getRocketAPI',
   async () => {
     const resp = await axios.get(APIurl);
     return resp.data;
@@ -21,35 +21,16 @@ const RocketSlice = createSlice({
   name: 'rockets',
   initialState,
   reducers: {
-    reserveRocket: {
-      reducer: (state, action) => state.map((item) => (
-        item.rocketId === action.payload ? { ...item, rocketReserved: true } : item
-      )),
-      prepare: (rocketId) => ({
-        payload: rocketId,
-      }),
-    },
-    cancelRocket: {
-      reducer: (state, action) => state.map((item) => (
-        item.rocketId === action.payload ? { ...item, reserveRocket: false } : item
-      )),
-      prepare: (rocketId) => ({
-        payload: rocketId,
-      }),
-    },
+    reserveRocket: (state, action) => state.map((item) => (
+      item.rocketId === action.payload ? { ...item, rocketReserved: true } : item)),
   },
+  cancelRocket: (state, action) => state.map((item) => (
+    item.rocketId === action.payload ? { ...item, reserveRocket: false } : item
+  )),
 
-  extraReducers: {
-    [getRocketAPI.fulfilled]: (state, action) => {
-      const rockets = action.payload.map((item) => ({
-        rocketId: item.rocket_id,
-        rocketName: item.rocket_name,
-        rocketDesc: item.description,
-        rocketImage: item.flickr_images[0],
-        rocketWiki: item.wikipedia,
-      }));
-      return rockets;
-    },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getRocketAPI.fulfilled, (state, action) => action.payload);
   },
 });
 
