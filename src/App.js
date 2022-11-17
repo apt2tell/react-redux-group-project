ae5212e9ae98f0743d3d371d89dc0f41ce19c327
Okechukwu <okei.muokwugwo@outlook.com>
2022-11-18 00:13:19 +0100

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React from 'react';
 import { Route, Routes } from 'react-router';
 import './App.css';
--- src/components/__tests__/Navbar.test.js
+++ src/components/__tests__/Navbar.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import { render } from '@testing-library/react';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
