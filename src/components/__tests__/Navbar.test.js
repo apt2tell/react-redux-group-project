28e7baf3b3ea6502b85d0a97086acc2b2445f339
x <exit>
2022-11-17 11:15:14 +0000

    Add tests

--- src/components/__tests__/Navbar.test.js
+++ src/components/__tests__/Navbar.test.js
@@ -1,8 +1,13 @@
 import { render } from '@testing-library/react';
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import Navbar from '../Navigation/Navbar';
 
 it('Navbar renders correctly', () => {
-  const Component = render(<Navbar />);
+  const Component = render(
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>,
+  );
   expect(Component).toMatchSnapshot();
 });
